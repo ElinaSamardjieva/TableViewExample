173cdb1061435fd0e704ced9f5d061bcde30061e
ElinaSamardjieva <elina.sm2@gmail.com>
2016-04-05 11:40:38 +0300

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Elina Samardjieva on 4/5/16.
+//  Copyright © 2016 Elina Samardjieva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
